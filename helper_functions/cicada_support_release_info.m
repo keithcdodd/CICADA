b835007362f7a05b861fed71b4e684645f02c573
keithcdodd <keithcdodd@gmail.com>
2026-07-23 15:13:29 -0600

    Finalize CICADA v1.1.0 release documentation

--- helper_functions/cicada_support_release_info.m
+++ helper_functions/cicada_support_release_info.m
@@ -1,7 +1,7 @@
 function info = cicada_support_release_info()
 % CICADA_SUPPORT_RELEASE_INFO Provenance for the support/reliability rc1 patch.
 info = struct();
-info.ReleaseTag = 'CICADA_v1.1.0-rc1_support_reliability';
+info.ReleaseTag = 'CICADA_v1.1.0_support_reliability';
 info.BaselineRepository = 'keithcdodd/CICADA';
 info.BaselineBranch = 'main';
 info.BaselineCommit = '3d07a40';
